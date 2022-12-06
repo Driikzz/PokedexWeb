1a671a2afbe103d88a1208b43891e077069f0bc9
Rémi Salles <remisalles.12@gmail.com>
2022-12-06 15:33:15 +0100

    Il faut faire le insert dans pokedex

--- index.js
+++ index.js
@@ -5,15 +5,27 @@ const port = 4444;
 
 dbo.connectToServer();
 
-/* index.js code before... */
+//GET pokemon
 app.get("/pokemon/list", function (req, res) {
-//on se connecte à la DB MongoDB
     const dbConnect = dbo.getDb();
-//premier test permettant de récupérer mes pokemons !
     dbConnect
         .collection("pokemon")
-        .find({}) // permet de filtrer les résultats
-        /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
+        .find({})
+        .toArray(function (err, result) {
+        if (err) {
+            res.status(400).send("Error fetching pokemons!");
+        } else {
+            res.json(result);
+        }
+        });
+});
+
+//GET pokedex 
+app.get("/pokedexUser/list", function (req, res) {
+    const dbConnect = dbo.getDb();
+    dbConnect
+        .collection("pokedex")
+        .find({})
         .toArray(function (err, result) {
         if (err) {
             res.status(400).send("Error fetching pokemons!");
@@ -29,7 +41,7 @@ const { set } = require("express/lib/application");
 app.use(bodyParser.urlencoded({ extended: true }));
 const jsonParser = bodyParser.json();
 
-
+//DELETE pokemon
 app.delete('/pokemon/delete',jsonParser,(req,res) =>{
     const body = req.body;
     const dbConnect = dbo.getDb();
@@ -37,7 +49,7 @@ app.delete('/pokemon/delete',jsonParser,(req,res) =>{
     console.log('Delete:', body);
     res.json(body);
 });
-
+//UPDATE pokemon
 app.post('/pokemon/update', jsonParser, (req, res) => {
     const body = req.body;
     const dbConnect = dbo.getDb();
@@ -47,14 +59,22 @@ app.post('/pokemon/update', jsonParser, (req, res) => {
     console.log('Changement de:', body);
 });
 
-
+//INSERT pokemon
 app.post('/pokemon/insert', jsonParser, (req, res) => {
     const body = req.body;
     const dbConnect = dbo.getDb();
     dbConnect.collection('pokemon').insert({...body});
     console.log('Got body:', body);
     res.json(body);
-    
+});
+
+//INSERT pokemon IN pokedex
+app.post('/pokedex/insert', jsonParser, (req, res) => {
+    const body = req.body;
+    const dbConnect = dbo.getDb();
+    selectPokemon = {name:body.selectPokemon};
+    dbConnect.collection('pokedex').insertOne(selectPokemon);
+    console.log('Pokemon capturé:', body);
 });
 
 app.listen(port, function () {
